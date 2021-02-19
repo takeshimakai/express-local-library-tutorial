2680b76e907698db685eca9653df291242dc1835
Kai Takeshima <takeshimakai@gmail.com>
2021-02-19 11:17:14 -0500

    prepare for deployment on Heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://***REMOVED***:***REMOVED***@cluster0.raaaq.mongodb.net/local_library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://***REMOVED***:***REMOVED***@cluster0.raaaq.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
